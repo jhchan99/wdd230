37fe330bfeb16c887a78bb760a2abecc08f2f5fc
jhchan99 <jhchan99@gmail.com>
2022-10-15 17:02:02 -0600

    lots of stuff

---
+++ BoM/mob.js
@@ -0,0 +1,37 @@
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const alert = document.querySelector('#favchap')
+
+button.addEventListener('click', function() {
+
+    if (input.value.length === 0) {
+        console.log(alert.setAttribute("placeholder", "Please select a chapter"));
+    }
+
+    else {
+
+    let myItem = input.value;
+
+        const listItem = document.createElement('li');
+        const listText = document.createElement('span');
+        const listBtn  = document.createElement('button');
+
+        listItem.appendChild(listText);
+        listText.textContent = myItem;
+        listItem.appendChild(listBtn);
+        listBtn.textContent = "Remove Content";
+    
+        list.appendChild(listItem);
+
+    input.value = "";
+
+    listBtn.addEventListener('click', function () {
+        list.removeChild(listItem);
+    });
+
+    };
+
+    input.focus();
+
+});
