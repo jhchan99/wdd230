abad0b0e8d65d41f1a4831b9771d93a1435fbbf0
jhchan99 <jhchan99@gmail.com>
2022-11-02 17:55:45 -0600

    finished form activity

--- chamber/js/index.js
+++ chamber/js/index.js
@@ -1,48 +1,48 @@
 let daynames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ];
-  
-  let d = new Date();
-  let dayName = daynames[d.getDay()];
-  let monthName = months[d.getMonth()];
-  let fulldate = dayName + ", "+ d.getDate() + " " + monthName + ", " + d.getFullYear();
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
 
-  document.getElementById("currentdate").textContent = fulldate;
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
 
-  document.getElementById("currentyear").textContent = d.getFullYear();
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fulldate = dayName + ", " + d.getDate() + " " + monthName + ", " + d.getFullYear();
+
+document.getElementById("currentdate").textContent = fulldate;
+
+document.getElementById("currentyear").textContent = d.getFullYear();
 
 
-  let oLastModif = new Date(document.lastModified);
-  document.getElementById("lastModif").textContent = oLastModif;
+let oLastModif = new Date(document.lastModified);
+document.getElementById("lastModif").textContent = oLastModif;
 
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
 
 // To solve the mid resizing issue with responsive class on
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+window.onresize = () => { if (window.innerWidth > 760) mainnav.classList.remove('responsive') };
 
 if (dayName == "Monday" || dayName == "Tuesday") {
 
