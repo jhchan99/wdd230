dc025a8f5aa16a998144e44805131f4fddc2a727
jhchan99 <jhchan99@gmail.com>
2022-11-17 12:38:04 -0700

    working

--- w10activity/weatherapi.js
+++ w10activity/weatherapi.js
@@ -3,7 +3,7 @@ const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("figcaption");
 
 const url =
-  "https://api.openweathermap.org/data/2.5/weather?lat=64.8378&lon=-147.7164&appid=532c6642b652353eefe1bf815d1cc36a";
+  "https://api.openweathermap.org/data/2.5/weather?lat=64.8378&lon=-147.7164&appid=532c6642b652353eefe1bf815d1cc36a&units=Imperial";
 
 async function apiFetch() {
   try {
@@ -11,7 +11,7 @@ async function apiFetch() {
     if (response.ok) {
       const data = await response.json();
       console.log(data); // this is for testing the call
-      // displayResults(data);
+      displayResults(data);
     } else {
       throw Error(await response.text());
     }
@@ -21,3 +21,25 @@ async function apiFetch() {
 }
 
 apiFetch();
+
+// function capitalizeFirstLetter(str) {
+//   // converting first letter to uppercase
+//   for (var i = 0; i < str.length; i++) {
+//     const capitalized = str.charAt(0).toUpperCase() + str.slice(1);
+
+//     return capitalized;
+//   }
+// }
+
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(
+    0
+  )}</strong>`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", desc);
+  captionDesc.textContent = desc.toUpperCase();
+}
