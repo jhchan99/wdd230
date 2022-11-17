078cfea4c22ce64254e980730afef2d57c2d07a3
jhchan99 <jhchan99@gmail.com>
2022-11-16 18:04:12 -0700

    small fixes

--- w10activity/weatherapi.js
+++ w10activity/weatherapi.js
@@ -3,7 +3,7 @@ const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("figcaption");
 
 const url =
-  "https://api.openweathermap.org/data/3.0/onecall?q=Fairbanks&units=imperial&appid=51aa657a579d41a2c5eb9285bb694cc4";
+  "https://api.openweathermap.org/data/2.5/weather?lat=64.8378&lon=-147.7164&appid=532c6642b652353eefe1bf815d1cc36a";
 
 async function apiFetch() {
   try {
