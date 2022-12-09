56de78655a6819ace58d87843f76c24ff188de32
jhchan99 <jhchan99@gmail.com>
2022-12-09 00:49:14 -0700

    changes to small view and other js

--- final/scripts/fruitpicker.js
+++ final/scripts/fruitpicker.js
@@ -10,22 +10,10 @@ fetch(requestURL)
     .then(function (jsonObject) {
         console.table(jsonObject);
         const directory = jsonObject;
-        directory.forEach(populateSelector);
         directory.forEach(populateSelector1);
         directory.forEach(populateSelector2);
+        directory.forEach(populateSelector3);
     });
-
-function populateSelector(directory) {
-    let fruit = document.createElement("option");
-
-    //change value of option element to match fruit name
-    fruit.value = `${directory.name}`;
-
-    //change textContent to property of option element to contain the directorys fruit names
-    fruit.textContent = `${directory.name}`;
-
-    document.getElementById("fruit").appendChild(fruit);
-}
 
 function populateSelector1(directory) {
     let fruit = document.createElement("option");
@@ -49,4 +37,61 @@ function populateSelector2(directory) {
     fruit.textContent = `${directory.name}`;
 
     document.getElementById("fruit2").appendChild(fruit);
+}
+
+function populateSelector3(directory) {
+    let fruit = document.createElement("option");
+
+    //change value of option element to match fruit name
+    fruit.value = `${directory.name}`;
+
+    //change textContent to property of option element to contain the directorys fruit names
+    fruit.textContent = `${directory.name}`;
+
+    document.getElementById("fruit3").appendChild(fruit);
+}
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const data = new FormData(event.target);
+
+    const value = Object.fromEntries(data.entries());
+
+    console.log({ value });
+
+    createTable(value);
+}
+
+
+
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit, createTable);
+
+
+function createTable(values) {
+    // create table elements to add to document
+    let row = document.createElement("tr");
+    let name = document.createElement("td");
+    let fruit1 = document.createElement("td");
+    let fruit2 = document.createElement("td");
+    let fruit3 = document.createElement("td");
+    let info = document.createElement("td");
+    let carbs = document.createElement("td");
+
+    name.textContent = `${values.fname}`;
+    fruit1.textContent = `${values.fruit1}`;
+    fruit2.textContent = `${values.fruit2}`;
+    fruit3.textContent = `${values.fruit3}`;
+    info.textContent = `${values.extrainfo}`
+
+    row.appendChild(name);
+    row.appendChild(fruit1);
+    row.appendChild(fruit2);
+    row.appendChild(fruit3);
+    row.appendChild(info);
+
+    document.querySelector("#information").appendChild(row);
+
 }
